947298567f596af6caa053ff241e97556d8c6891
Peter Lenon <goshomip@gmail.com>
2023-08-18 23:38:41 -0500

    awaiting API intergration

--- SignalGenerator.py
+++ SignalGenerator.py
@@ -4,31 +4,52 @@ import numpy as np
 
 
 # if signal > 0(+ve) then its a buy signal else if a signal < 0 the its a sell signal else do nothing
-def Buy_or_Sell_Signal(data):
+def Buy_or_Sell_Signal(dataframe):
     def nearest_supp_and_res_lines(candle):
         Open = np.array(candle.Open)[0]
         Close = np.array(candle.Close)[0]
         High = np.array(candle.High)[0]
         Low = np.array(candle.Low)[0]
-        lines = [-1, -1, -1]
+        lines = [-1, -1]
         for line in support_and_resistance_lines:
             if line in range(Close, Open):
                 lines[1] = line
                 return lines
 
         left, right = 0, len(support_and_resistance_lines)-1
+        res = -1
+        sup = -1
         while left <= right:
-            if Low <= support_and_resistance_lines[left]: # this logic requires use of pip ranges not raw numbers
-                pass
+            if Close > Open:
+                if support_and_resistance_lines[left] <= (Open + (Close-Open)/2):
+                    res = left
+                if support_and_resistance_lines[right] >= (Open + (Close - Open)/2):
+                    sup = right
+            else:
+                if support_and_resistance_lines[left] <= (Close +(Open - Close)/2):
+                    res = left
+                if support_and_resistance_lines[right] >= (Close +(Open - Close)/2):
+                    sup = right
+            left += 1
+            right -= 1
+        lines = [res, sup]
+        return lines
 
 
-
-
-
-    support_and_resistance_lines = get_Support_and_Resistance(dataframe=data,
-                                                              optimal_cluster_size=choose_num_of_clusters(data))
-    candle_stick_patterns = pattern_Signals(data)
+    support_and_resistance_lines = get_Support_and_Resistance(dataframe=dataframe,
+                                                              optimal_cluster_size=choose_num_of_clusters(dataframe))
+    candle_stick_patterns = pattern_Signals(dataframe)
     sentiment_index = sum(signal for signal in candle_stick_patterns)
+    last_candle = getLastCandle(dataframe)
+    supp_and_res = nearest_supp_and_res_lines(last_candle)
     if sentiment_index > 0:
-        last_candle = getLastCandle(data)
+        support = supp_and_res[0]
+        if abs(np.array(last_candle.Lowest)[0] - support) <= (np.array(last_candle.Close)[0] + np.array(np.array(last_candle.Open))[0])/2 - np.array(last_candle.Low)[0]:
+            return 1
+    elif sentiment_index < 0:
+        res = supp_and_res[1]
+        if abs(res - np.array(last_candle.Highest)[0]) <= np.array(last_candle.Highest)[0] - (np.array(last_candle.Close)[0] + np.array(last_candle)[0])/2:
+            return -1
+    return 0
+
 
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import yfinance as yf                           #primarily for stocks, funds, indices etc.
 import pandas as pd
 from datetime import datetime, timedelta
-from Support_and_Resistance import choose_num_of_clusters, get_Support_and_Resistance, plot_clusters
-from CandleStick_Patterns import DragonFly_Doji
+from SignalGenerator import Buy_or_Sell_Signal
 
 def fetchData(currency_pair, candles_back, interval): # pass string , int, string # 'EURUSD-X', 200, '15m'
     timeframe = interval[len(interval) - 1]
@@ -14,10 +13,28 @@ def fetchData(currency_pair, candles_back, interval): # pass string , int, strin
     dataframe = pd.DataFrame(data)
     return dataframe
 
-
 if __name__ == "__main__":
     data = fetchData("BTC", 200, '1d')
-    number_of_clusters = choose_num_of_clusters(data)
-    sup_and_res = get_Support_and_Resistance(data, number_of_clusters)
-    # plot_clusters(data, sup_and_res=sup_and_res)
-    # print(DragonFly_Doji(data))
+    Buy = False
+    Sell = False
+    Neither = False
+
+    while True:
+        signal = Buy_or_Sell_Signal(data)
+        if signal == 1:
+            Buy = True
+            Sell = False
+            Neither = False
+        elif signal == -1:
+            Buy = False
+            Sell = True
+            Neither = False
+        else:
+            Buy = False
+            Sell = False
+            Neither = True
+
+        #post(signal to the API) 
+
+
+
